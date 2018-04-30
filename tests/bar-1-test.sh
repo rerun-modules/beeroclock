ac77bc1f55a35d616820fa0eb3a156d67700693a
Lee Thompson <lee.thompson@precision-autonomy.com>
2018-04-30 16:57:39 -0500

    Add curl call to make sure container xinetd is working correctly

--- tests/bar-1-test.sh
+++ tests/bar-1-test.sh
@@ -12,29 +12,31 @@
 describe "bar"
 
 it_starts() {
-	rerun beeroclock:bar
-  	test -f ~/.beeroclock/container.txt
-  	.  ~/.beeroclock/container.txt
-	test "true" = "$(docker inspect -f '{{.State.Running}}' "$CONTAINERID")"
-	docker stop "$CONTAINERID"
-  	rm  ~/.beeroclock/container.txt
+    rerun beeroclock:bar --rebuild true
+    test -f ~/.beeroclock/container.txt
+    .  ~/.beeroclock/container.txt
+    test "true" = "$(docker inspect -f '{{.State.Running}}' "$CONTAINERID")"
+    sleep 2
+    curl http://127.0.0.1:28080/beer
+    docker stop "$CONTAINERID"
+    rm  ~/.beeroclock/container.txt
 }
 
 it_stops() {
-	rerun beeroclock:bar
-  	test -f ~/.beeroclock/container.txt
-  	.  ~/.beeroclock/container.txt
+    rerun beeroclock:bar
+    test -f ~/.beeroclock/container.txt
+    .  ~/.beeroclock/container.txt
 
-	rerun beeroclock:bar --close true
-  	! test -f ~/.beeroclock/container.txt
-  	status=$(docker inspect -f '{{.State.Running}}' "$CONTAINERID")
-	case "$status" in
-		true)
-			docker stop "$CONTAINERID"
-		  	rm  ~/.beeroclock/container.txt
-		;;
-		false)
-			echo "the service was stopped"
-		;;
-	esac
+    rerun beeroclock:bar --close true
+    ! test -f ~/.beeroclock/container.txt
+    status=$(docker inspect -f '{{.State.Running}}' "$CONTAINERID")
+    case "$status" in
+        true)
+            docker stop "$CONTAINERID"
+            rm  ~/.beeroclock/container.txt
+        ;;
+        false)
+            echo "the service was stopped"
+        ;;
+    esac
 }
